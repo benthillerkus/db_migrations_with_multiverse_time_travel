8b5d279e62c54568795b1ad648acf99d9e2a2807
Bent Hillerkus <bent@bent.party>
2025-03-06 17:45:03 +0100

    ci: fix loading sqlite3

--- packages/sqlite3/test/sqlite3_migrations_with_multiverse_timetravel_test.dart
+++ packages/sqlite3/test/sqlite3_migrations_with_multiverse_timetravel_test.dart
@@ -1,18 +1,7 @@
-import 'dart:ffi';
-// import 'dart:io';
-
-import 'package:sqlite3/open.dart';
 import 'package:sqlite3/sqlite3.dart';
 import 'package:test/test.dart';
-// import 'package:path/path.dart';
 
 void main() {
-  setUpAll(() {
-    open.overrideFor(OperatingSystem.linux, () {
-      return DynamicLibrary.open('sqlite3.so');
-    });
-  });
-
   late Database db;
 
   setUp(() {
