ca3e8ce22a902c796bc402b07bac486314dc027b
Bent Hillerkus <bent@bent.party>
2025-03-06 17:24:02 +0100

    chore: fix warnings

--- packages/sqlite3/test/sqlite3_migrations_with_multiverse_timetravel_test.dart
+++ packages/sqlite3/test/sqlite3_migrations_with_multiverse_timetravel_test.dart
@@ -2,9 +2,7 @@ import 'dart:ffi';
 import 'dart:io';
 
 import 'package:sqlite3/open.dart';
-import 'package:sqlite3_migrations_with_multiverse_timetravel/sqlite3_migrations_with_multiverse_timetravel.dart';
 import 'package:sqlite3/sqlite3.dart';
-import 'package:sqlite3/open.dart';
 import 'package:test/test.dart';
 import 'package:path/path.dart';
 
