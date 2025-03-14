5a46d52e5b751ae2ae50c211cc7bba677423ab12
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2025-03-14 19:05:58 +0100

    test: add some more tests

--- packages/generic/test/algorithm_test.dart
+++ packages/generic/test/algorithm_test.dart
@@ -48,4 +48,33 @@ void main() {
 
     expect(() => migrator.call(db: db, defined: defined.iterator), throwsA(isA<StateError>()));
   });
+
+  test('Rollback no common', () {
+    final db = MockDatabase([
+      Migration(definedAt: DateTime(2025, 3, 6), up: null, down: null),
+      Migration(definedAt: DateTime(2025, 3, 7), up: null, down: null),
+      Migration(definedAt: DateTime(2025, 3, 8), up: null, down: null),
+    ]);
+
+    Migrator<Null>().call(db: db, defined: <Migration<Null>>[].iterator);
+
+    expect(db.applied, isEmpty);
+  });
+
+  test('Rollback some common', () {
+    final defined = [
+      Migration(definedAt: DateTime(2025, 3, 6), up: null, down: null),
+      Migration(definedAt: DateTime(2025, 3, 9), up: null, down: null),
+    ];
+
+    final db = MockDatabase([
+      Migration(definedAt: DateTime(2025, 3, 6), up: null, down: null),
+      Migration(definedAt: DateTime(2025, 3, 7), up: null, down: null),
+      Migration(definedAt: DateTime(2025, 3, 8), up: null, down: null),
+    ]);
+
+    Migrator<Null>().call(db: db, defined: defined.iterator);
+
+    expect(IterableEquality().equals(db.applied, defined), isTrue);
+  });
 }
