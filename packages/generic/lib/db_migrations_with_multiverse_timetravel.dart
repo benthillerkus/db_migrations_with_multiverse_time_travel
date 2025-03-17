65fea57edeac21b3fab70329c3ee65e0e2aa6206
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2025-03-17 17:52:18 +0100

    chore: remove top-level sample library documentation

--- packages/generic/lib/db_migrations_with_multiverse_timetravel.dart
+++ packages/generic/lib/db_migrations_with_multiverse_timetravel.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
 export 'src/migration.dart';
 export 'src/database.dart';
 export 'src/algorithm.dart';
--- packages/sqlite3/lib/sqlite3_migrations_with_multiverse_timetravel.dart
+++ packages/sqlite3/lib/sqlite3_migrations_with_multiverse_timetravel.dart
@@ -1,8 +1,2 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
 export 'src/database.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'package:db_migrations_with_multiverse_timetravel/src/migration.dart';
