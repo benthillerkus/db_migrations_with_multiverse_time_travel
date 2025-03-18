78b107a473209fb782043177b29c3ebb14d67c29
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2025-03-18 04:22:18 +0100

    chore: add a stupid change

--- packages/sqlite3/lib/sqlite3_migrations_with_multiverse_time_travel.dart
+++ packages/sqlite3/lib/sqlite3_migrations_with_multiverse_time_travel.dart
@@ -1,3 +1,6 @@
+/// A library for managing SQLite database migrations with multiverse time travel.
+library;
+
 export 'package:db_migrations_with_multiverse_time_travel/db_migrations_with_multiverse_time_travel.dart' show Migration, SyncMigrateExt;
 
 export 'src/database.dart';
