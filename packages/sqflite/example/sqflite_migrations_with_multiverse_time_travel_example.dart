9f6b07d424c51a6696afdbdf51e42eaec9a63783
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2025-03-22 06:13:37 +0100

    chore: initial commit

---
+++ packages/sqflite/example/sqflite_migrations_with_multiverse_time_travel_example.dart
@@ -0,0 +1,6 @@
+import 'package:sqflite_migrations_with_multiverse_time_travel/sqflite_migrations_with_multiverse_time_travel.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/sqflite/test/sqflite_migrations_with_multiverse_time_travel_test.dart
@@ -0,0 +1,16 @@
+import 'package:sqflite_migrations_with_multiverse_time_travel/sqflite_migrations_with_multiverse_time_travel.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
